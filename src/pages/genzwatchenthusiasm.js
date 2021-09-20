1a76f6ea02b1fd05fdb11e522e4660c63482a181
djlisbonne <55104821+djlisbonne@users.noreply.github.com>
2021-09-20 15:40:04 -0400

    Small tweaks

--- src/pages/genzwatchenthusiasm.js
+++ src/pages/genzwatchenthusiasm.js
@@ -11,27 +11,29 @@ const Link8 = () => (
   <Layout>
     <Seo title="Story #8" />
     <div class="article">
-        <h1><Link to="/coding/"><BsArrowLeftShort/></Link>Being a watch enthusiast in the generation that doesn't wear watches</h1>
+        <h1><Link to="/writing/"><BsArrowLeftShort/></Link>Being a watch enthusiast in the generation that doesn't wear watches</h1>
         <h5>By David Lisbonne | 4.22.20</h5>
         <hr/>
-        <section>
-            <p> <i>“What’s so hard about looking at your phone?” </i></p>
-            <p> The stereotype that GenZ doesn’t wear watches is largely true. There is no need, say my friends, of a device on my wrist that tells me less information than the device in my hand; of a device on my wrist that’s less readable than the device with a 6 inch OLED screen; of a device that feels clunky, gets caught on shirts, and serves no valuable purpose. </p>
-            <p>However, as the title suggests, I am a watch enthusiast. As a young college student, I have a collection that’s exactly on par with what you’re imagining right now. It’s not large, it’s not deep, but it is mine. It’s the result of saving paychecks, spending countless hours in the depths of watch forums, serial number lookup tables and Michigan’s fine arts library (they’ve got some great books on watchmaking) concluded with that nerve-wracking moment you win the auction or click the Submit Order button and think “I can’t believe I just did that.” </p>
-            <p>I’m sure my friends will say I go on and on about watches. On the contrary, I feel like I’m only divulging 10%. I show my friends photos of watches to get their opinions, try to explain why one watch costs 100x another, and why they’re such incredible machines. The result is typically an eyes-glazed-over slouch, recessing further into the couch and away from the noise (aka me).</p>
-            <p>This is all to say that despite my best efforts, my friends aren’t interested in watches, and the apathy manifests itself physically, as they don’t wear watches. Well, not real watches. My friends are all athletes –– soccer, basketball, running, swimming, cycling, and more –– so their wrists are adorned with Apple Watches and Garmins. I myself have a Garmin Fenix 5x, and it’s incredible (I’m a runner and a huge backpacker). But it has no soul, no personality, no character. It’s just a machine.</p>
-            <p>But I’m not too broken up about this trend. I’m optimistic that smartwatch dominance bodes well for a futures enthusiasts: I’d bet that the young adult wearing an Apple Watch today is more likely to don a mechanical beauty in 20 years than the young adult with a naked wrist. I might be wrong, but I’m hopeful it’ll hold true.</p>
-            <p>I’m not trying to win converts on a deadline. I’m just trying to do my part in keeping watches going, and given my generational positioning, my battle plan is as follows.</p>
-            <p>“Kids, (read: people my age) consider buying an Apple Watch, Garmin or some other smartwatch. They provide amazing functionality, are super comfortable and a great bang for your buck.”</p>
-            <p>Honestly, it’s already started holding true. My roommate asked for advice when looking into an Apple Watch, and now absolutely loves it. Another good friend of mine has had every Apple Watch since they were introduced, but over lunch the other day mentioned he’s looking into buying a mechanical watch — he even started watching the Talking Watches episodes from HODINKEE!</p>
-            <p>As modern technology continues to innovate and revolutionize nearly all aspects of life, I truly believe the need for a smartwatch will begin to decline. Consider smart glasses, IoT in our homes, cars that interface with social and work related services, and even more powerful virtual assistants combined with accessories (imagine Siri in 15–20 years + AirPods v.18); all of these innovations could conceivably lead to an even broader resurgence in mechanical watches as we yearn for a personal expression that’s got a taste of vintage.</p>
-            <p>And of every 16–24 year old today, who do you have your money on to buy a Vulcain Cricket in 20 years? The kid with the naked wrist, or the kid with an Apple Watch?</p>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-        </section>
+        <div class="story">
+          <section>
+              <p> <i>“What’s so hard about looking at your phone?” </i></p>
+              <p> The stereotype that GenZ doesn’t wear watches is largely true. There is no need, say my friends, of a device on my wrist that tells me less information than the device in my hand; of a device on my wrist that’s less readable than the device with a 6 inch OLED screen; of a device that feels clunky, gets caught on shirts, and serves no valuable purpose. </p>
+              <p>However, as the title suggests, I am a watch enthusiast. As a young college student, I have a collection that’s exactly on par with what you’re imagining right now. It’s not large, it’s not deep, but it is mine. It’s the result of saving paychecks, spending countless hours in the depths of watch forums, serial number lookup tables and Michigan’s fine arts library (they’ve got some great books on watchmaking) concluded with that nerve-wracking moment you win the auction or click the Submit Order button and think “I can’t believe I just did that.” </p>
+              <p>I’m sure my friends will say I go on and on about watches. On the contrary, I feel like I’m only divulging 10%. I show my friends photos of watches to get their opinions, try to explain why one watch costs 100x another, and why they’re such incredible machines. The result is typically an eyes-glazed-over slouch, recessing further into the couch and away from the noise (aka me).</p>
+              <p>This is all to say that despite my best efforts, my friends aren’t interested in watches, and the apathy manifests itself physically, as they don’t wear watches. Well, not real watches. My friends are all athletes –– soccer, basketball, running, swimming, cycling, and more –– so their wrists are adorned with Apple Watches and Garmins. I myself have a Garmin Fenix 5x, and it’s incredible (I’m a runner and a huge backpacker). But it has no soul, no personality, no character. It’s just a machine.</p>
+              <p>But I’m not too broken up about this trend. I’m optimistic that smartwatch dominance bodes well for a futures enthusiasts: I’d bet that the young adult wearing an Apple Watch today is more likely to don a mechanical beauty in 20 years than the young adult with a naked wrist. I might be wrong, but I’m hopeful it’ll hold true.</p>
+              <p>I’m not trying to win converts on a deadline. I’m just trying to do my part in keeping watches going, and given my generational positioning, my battle plan is as follows.</p>
+              <p>“Kids, (read: people my age) consider buying an Apple Watch, Garmin or some other smartwatch. They provide amazing functionality, are super comfortable and a great bang for your buck.”</p>
+              <p>Honestly, it’s already started holding true. My roommate asked for advice when looking into an Apple Watch, and now absolutely loves it. Another good friend of mine has had every Apple Watch since they were introduced, but over lunch the other day mentioned he’s looking into buying a mechanical watch — he even started watching the Talking Watches episodes from HODINKEE!</p>
+              <p>As modern technology continues to innovate and revolutionize nearly all aspects of life, I truly believe the need for a smartwatch will begin to decline. Consider smart glasses, IoT in our homes, cars that interface with social and work related services, and even more powerful virtual assistants combined with accessories (imagine Siri in 15–20 years + AirPods v.18); all of these innovations could conceivably lead to an even broader resurgence in mechanical watches as we yearn for a personal expression that’s got a taste of vintage.</p>
+              <p>And of every 16–24 year old today, who do you have your money on to buy a Vulcain Cricket in 20 years? The kid with the naked wrist, or the kid with an Apple Watch?</p>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+          </section>
+        </div>
     </div>
   </Layout>
 )
